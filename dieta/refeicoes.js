37932fb27d3e5edceac87eae8805f4f28bd348f3
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2023-02-24 23:46:44 -0300

    Create refeicoes.js

---
+++ dieta/refeicoes.js
@@ -0,0 +1,257 @@
+export default {
+  "1": {
+    "desjejum": {
+      "main": [
+        "180ml Suco de Fruta Natural ou Integral ou Polpa",
+        "Crepioca Crocante:\n20g Tapioca goma\n10g polvilho azedo\n1 ovo\nmisturar bem os ingredientes até amassa ficar homogênea, despejar em frigideira teflon pré aquecida, grelhar os dois lados\nRechear com Creme Cottage + 1 ovo"
+      ],
+      "variation": [
+        "180ml Suco de Fruta Natural ou Integral ou Polpa",
+        "1 Fatia Pão de Forma Integral",
+        "70g Frango ou 50g Atum"
+      ]
+    },
+    "colacao": [
+      "1 unidade Fruta(100g- 130g)",
+      "20g de aveia em flocos",
+      "180ml Leite Semidesnatado"
+    ],
+    "almoco": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "ACRESCENTAR 1 COLHER DE SOPA DE SEMENTES(GERGELIM ou ABOBORA ou GIRASSOL)",
+      "100g Arroz ou 130g Batata Doce ou 250g Batata Inglesa ou 100g Mandioca ou 110g Macarrão Integral.",
+      "85g Feijão ou Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)",
+      "1 unidade Fruta ou 200ml Suco de Fruta Natural ou polpa de Fruta"
+    ],
+    "lanche": {
+      "main": [
+        "1 unidade Fruta",
+        "1 Iogurte Desnatado(+ ½ scoop Whey Protein)",
+        " 2 Fatias Pão de Forma Integral com cottage ou Requeijão"
+      ],
+      "variation": [
+        "1 unidade Fruta",
+        "20g Aveia em Flocos + 10g me",
+        "1 scoop Whey Protein + 200ml agua",
+        "1 Fatia Pão de Forma Integral"
+      ]
+    },
+    "jantar": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "Temperar a Salada todos os dias com azeite extra virgem",
+      "75g Arroz ou 100g Batata Doce ou 190g Batata Inglesa ou 80g Mandioca ou 80g Macarrão Integral.",
+      "3 col. de sopa de Feijão Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)"
+    ]
+  },
+  "2": {
+    "desjejum": {
+      "main": [
+        "- 180ml Suco de Fruta Natural ou Integral ou Polpa",
+        "1 Fatia Pão de Forma Integral",
+        "2 ovos"
+      ],
+      "variation": [
+        "180ml Suco de Fruta Natural ou Integral ou Polpa",
+        "Omelete ( 2 ovos + 1 col. de sopa de Linhaça Triturada + uma pitada de Sal) "
+      ]
+    },
+    "colacao": [
+      "2 unidades Castanha do Pará",
+      "1 Fatia Queijo Branco, tipo minas frescal(30g)"
+    ],
+    "almoco": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "ACRESCENTAR 1 COLHER DE SOPA DE SEMENTES(GERGELIM ou ABOBORA ou GIRASSOL)",
+      "100g Arroz ou 130g Batata Doce ou 250g Batata Inglesa ou 100g Mandioca ou 110g Macarrão Integral.",
+      "85g Feijão ou Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)",
+      "1 unidade Fruta ou 200ml Suco de Fruta Natural ou polpa de Fruta"
+    ],
+    "lanche": {
+      "main": ["1 unidade Pão Francês", "2 ovos mexido", "1 unidade Fruta"],
+      "variation": [
+        "2 Fatias Pão de Forma Integral",
+        "70g Frango ou 50g Atum",
+        "1 unidade Fruta"
+      ]
+    },
+    "jantar": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "Temperar a Salada todos os dias com azeite extra virgem",
+      "75g Arroz ou 100g Batata Doce ou 190g Batata Inglesa ou 80g Mandioca ou 80g Macarrão Integral.",
+      "3 col. de sopa de Feijão Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)"
+    ]
+  },
+  "3": {
+    "desjejum": {
+      "main": [
+        "180ml Suco de Fruta Integral ou Natural ou de polpa",
+        "Aveioca: 30g Aveia em Flocos finos \n 3 col. de sopa de agua ( 15ml de agua) \n 1 ovo\n orégano e sal\n misturar bem os ingredientes até a massa tornar homogenea, despejar na frigideira pré aquecida, grelhar os dois lados.\n Pode rechear com Creme Cottage \n 1 ovo "
+      ],
+      "variation": []
+    },
+    "colacao": ["1 unidade Fruta(100g-130g)", "25g granola"],
+    "almoco": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "ACRESCENTAR 1 COLHER DE SOPA DE SEMENTES(GERGELIM ou ABOBORA ou GIRASSOL)",
+      "100g Arroz ou 130g Batata Doce ou 250g Batata Inglesa ou 100g Mandioca ou 110g Macarrão Integral.",
+      "85g Feijão ou Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)",
+      "1 unidade Fruta ou 200ml Suco de Fruta Natural ou polpa de Fruta"
+    ],
+    "lanche": {
+      "main": [
+        "1 unidade Fruta",
+        "1 Iogurte Desnatado(+ ½ scoop Whey Protein)",
+        "2 Fatias Pão de Forma Integral com cottage ou Requeijão"
+      ],
+      "variation": [
+        "Panqueca de Coco:\n1 scoop Whey\n3 claras\n2 col. de sopa Coco ralado\nse achar necessário umas gotinhas de adoçante\n-Misture tudo em uma caneca com o auxilio de um garfo e depois coloque na frigideira antiaderente para assar dos dois lados\nsalpicar canela na hora de comer"
+      ]
+    },
+    "jantar": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "Temperar a Salada todos os dias com azeite extra virgem",
+      "75g Arroz ou 100g Batata Doce ou 190g Batata Inglesa ou 80g Mandioca ou 80g Macarrão Integral.",
+      "3 col. de sopa de Feijão Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)"
+    ]
+  },
+  "4": {
+    "desjejum": {
+      "main": [
+        "180ml Suco de Fruta Natural ou Integral ou Polpa",
+        "Omelete:\n2 ovos\n1 col. de sopa de Linhaça Triturada\numa pitada de Sal"
+      ],
+      "variation": []
+    },
+    "colacao": [],
+    "almoco": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "ACRESCENTAR 1 COLHER DE SOPA DE SEMENTES(GERGELIM ou ABOBORA ou GIRASSOL)",
+      "100g Arroz ou 130g Batata Doce ou 250g Batata Inglesa ou 100g Mandioca ou 110g Macarrão Integral.",
+      "85g Feijão ou Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)",
+      "1 unidade Fruta ou 200ml Suco de Fruta Natural ou polpa de Fruta"
+    ],
+    "lanche": {
+      "main": [
+        "1 unidade Fruta( 100g - 130g)",
+        "13 unidades Biscoito de Arroz Integral com Geleia de Fruta(dar preferência sem açúcar e natural queensberry 100% fruta)",
+        "2 Fatia Queijo Branco, tipo Minas Frescal sem lactose(60g)"
+      ],
+      "variation": [
+        "Smoothie de Morango e Banana",
+        "200 ml água de coco",
+        "1 scoop Whey Protein; sabor baunilha",
+        "1 banana congelada sem casca",
+        "6 morangos congelados",
+        "3 cubos de gelo",
+        "Bater todos os ingredientes no liquidificador e tomar em seguida.",
+        "1 Fatia Pão de Forma Integral"
+      ]
+    },
+    "jantar": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "Temperar a Salada todos os dias com azeite extra virgem",
+      "75g Arroz ou 100g Batata Doce ou 190g Batata Inglesa ou 80g Mandioca ou 80g Macarrão Integral.",
+      "3 col. de sopa de Feijão Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)"
+    ]
+  },
+  "5": {
+    "desjejum": {
+      "main": [
+        "180ml Suco de Fruta Natural ou Integral ou Polpa",
+        "Crepioca Crocante:\n20g Tapioca goma\n10g polvilho azedo\n1 ovo\nmisturar bem os ingredientes até amassa ficar homogênea, despejar em frigideira teflon pré aquecida, grelhar os dois lados\nRechear com Creme Cottage + 1 ovo"
+      ],
+      "variation": [
+        "180ml Suco de Fruta Natural ou Integral ou Polpa",
+        "1 Fatia Pão de Forma Integral",
+        "70g Frango ou 50g Atum"
+      ]
+    },
+    "colacao": [
+      "1 unidade Fruta(100g- 130g)",
+      "20g de aveia em flocos",
+      "180ml Leite Semidesnatado"
+    ],
+    "almoco": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "ACRESCENTAR 1 COLHER DE SOPA DE SEMENTES(GERGELIM ou ABOBORA ou GIRASSOL)",
+      "100g Arroz ou 130g Batata Doce ou 250g Batata Inglesa ou 100g Mandioca ou 110g Macarrão Integral.",
+      "85g Feijão ou Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)",
+      "1 unidade Fruta ou 200ml Suco de Fruta Natural ou polpa de Fruta"
+    ],
+    "lanche": {
+      "main": [
+        "1 unidade Fruta (100g - 130g)",
+        "2 unidades Castanha do Pará",
+        "1 Iogurte Desnatado (+ ½ scoop Whey)"
+      ],
+      "variation": [
+        "1 scoop Whey Protein + 200ml agua",
+        "1 unidade Fruta( 100- 130g)",
+        "25g GRanola"
+      ]
+    },
+    "jantar": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "Temperar a Salada todos os dias com azeite extra virgem",
+      "75g Arroz ou 100g Batata Doce ou 190g Batata Inglesa ou 80g Mandioca ou 80g Macarrão Integral.",
+      "3 col. de sopa de Feijão Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)"
+    ]
+  },
+  "6": {
+    "desjejum": {
+      "main": [
+        "-Suco Verde:\nSuco de 2 Laranjas \n2 Folha de Couves\n 1 col. de sobremesa de chia\n 180ml de agua\nbater tudo no liquidificador e tomar em seguida)",
+        "2 ovos mexido + 1 col. de sobremesa de Linhaça Triturada"
+      ],
+      "variation": []
+    },
+    "colacao": [],
+    "almoco": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "ACRESCENTAR 1 COLHER DE SOPA DE SEMENTES(GERGELIM ou ABOBORA ou GIRASSOL)",
+      "100g Arroz ou 130g Batata Doce ou 250g Batata Inglesa ou 100g Mandioca ou 110g Macarrão Integral.",
+      "85g Feijão ou Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)",
+      "1 unidade Fruta ou 200ml Suco de Fruta Natural ou polpa de Fruta"
+    ],
+    "lanche": {
+      "main": [
+        "Bolinho Chocolate: \n½ banana\n1 ovo\n1 Tamara\n 1 col. de sopa de Aveia\n1 col. de sopa de Farinha de Amêndoa\n1 col. de sopa de Cacau crioulo\n3 col. de sopa de Bebida Vegetal\nbata tudo no liquidificador, transfira para um refratário e colocar no microondas por 1’30”"
+      ],
+      "variation": [
+        "13 unidades Biscoito de Arroz Integral com Geleia de Fruta(dar preferência sem açúcar e naturalqueensberry 100% fruta)",
+        "2 Fatias Queijo Branco, tipo Minas Frescal sem lactose(60g)",
+        "180ml Suco de Fruta Natural ou Integral ou Polpa"
+      ]
+    },
+    "jantar": [
+      "Vegetal A - à vontade",
+      "Vegetal B - 6 col. De sopa",
+      "Temperar a Salada todos os dias com azeite extra virgem",
+      "75g Arroz ou 100g Batata Doce ou 190g Batata Inglesa ou 80g Mandioca ou 80g Macarrão Integral.",
+      "3 col. de sopa de Feijão Lentilha ou Soja",
+      "1 Porção Magra Carne(se for bovina 100g, se for frango 120g, se for suína 95g, se for peixe 150g e se for ovos 3 unidades)"
+    ]
+  }
+}
